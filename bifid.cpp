0d2156acf2d28fb1c8128ecd5140346d14150b0a
saurabh0719 <saurabhpuj99@gmail.com>
2019-12-16 11:25:53 +0530

    new commits

--- bifid.cpp
+++ bifid.cpp
@@ -1,3 +1,11 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+
+#define period 3
+
+char key_square = {{'R','A','N','C','H'},
+                   {'O','B','D','E','F'},
+                   {'G','I','K','L','M'},
+                   {'P','Q','S','T','U'},
+                   {'V','W','X','Y','Z'}};
