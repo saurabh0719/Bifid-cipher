52d882ae63b28ae582074ec8163674182a9c0ff0
saurabh0719 <saurabhpuj99@gmail.com>
2019-12-17 17:32:36 +0530

    added decryptor

--- bifid.cpp
+++ bifid.cpp
@@ -54,15 +54,15 @@ void encrypt()
     col.push_back(key_column(str[i]));
   }
 
-  for(int i =0; i<row.size(); i++)
+  /*for(int i =0; i<row.size(); i++)
   {
     cout<<row[i]<<" "<<col[i]<<endl;
-  }
+  }*/
 
   int len = row.size();
   len = len/period;
 
-  cout<<"Length/period : "<<len<<endl;
+  //cout<<"Length/period : "<<len<<endl;
 
   vector<int> row_col_merge;
 
@@ -87,12 +87,12 @@ void encrypt()
     increment += period;
   }
 
-  cout<<"Row column merge : "<<endl;
+  /*cout<<"Row column merge : "<<endl;
   for(int i =0; i<row_col_merge.size(); i++)
   {
     cout<<row_col_merge[i]<<" ";
   }
-  cout<<endl;
+  cout<<endl;*/
 
   curr = 0;
 
@@ -112,12 +112,12 @@ void encrypt()
     }
   }
 
-  cout<<"Row column merge after adding remaining elements: "<<endl;
+  /*cout<<"Row column merge after adding remaining elements: "<<endl;
   for(int i =0; i<row_col_merge.size(); i++)
   {
     cout<<row_col_merge[i]<<" ";
   }
-  cout<<endl;
+  cout<<endl;*/
 
   string result;
 
@@ -129,16 +129,80 @@ void encrypt()
   cout<<"The encrypted string is : "<<result<<endl;
 }
 
+void decrypt()
+{
+  string str;
+  cout<<"Enter the string to be decrypted : ";
+  cin>>str;
+
+  vector<int> row;
+  vector<int> col;
+  vector<int> row_col_merge;
+
+  for(int i =0; i<str.length(); i++)
+  {
+    row_col_merge.push_back(key_row(str[i]));
+    row_col_merge.push_back(key_column(str[i]));
+  }
+
+  int len = (row_col_merge.size())/period;
+  if(len%2 !=0)
+  len--;
+
+  int j =0;
+  int i =0;
+
+  for(i =0; i<row_col_merge.size() && len>0; i++)
+  {
+    j=0;
+    while(j<period)
+    {
+      row.push_back(row_col_merge[i]);
+      j++;
+    }
+    j=0;
+    while(j<period)
+    {
+      col.push_back(row_col_merge[j]);
+      j++;
+    }
+    len--;
+  }
+
+  len = (row_col_merge.size() - i)/2;
+
+  for(;i<=len;i++)
+  row.push_back(row_col_merge[i]);
+  for(;i<row_col_merge.size();i++)
+  col.push_back(row_col_merge[j]);
+
+  len = row.size();
+  string result;
+
+  for(i=0; i<len; i++)
+  {
+    result.push_back(key_square[row[i]][col[i]]);
+  }
+
+  cout<<"The decrypted string is : "<<result<<endl;
+
+}
+
 int main()
 {
 
   while(1)
   {
     int c;
-    cout<<"Encrypt? 0/1 : ";
+    cout<<"1. Encryption"<<endl;
+    cout<<"2. Decryption"<<endl;
+    cout<<"Any other key to Exit"<<endl;
+    cout<<"Enter a key : ";
     cin>>c;
     if(c==1)
     encrypt();
+    else if(c==2)
+    decrypt();
     else break;
   }
 
